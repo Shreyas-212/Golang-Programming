2744738010d5fccc4ec034ba47a9bd92d0cf26c4
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-01-27 20:23:53 +0530

    adding mutation_methodes code

---
+++ mutation_methods/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+func (u User)outputUserValues() {
+	fmt.Println(u.firstName, u.lastName, u.birthDate, u.createdAt)
+}
+
+func (u *User)clearUserValues() {
+	u.firstName = ""
+    u.lastName = ""
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthDate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser User 
+
+	appUser = User{
+		firstName: userFirstName,
+        lastName:  userLastName,
+        birthDate: userBirthDate,
+        createdAt: time.Now(),
+	}
+
+	appUser.outputUserValues()
+	appUser.clearUserValues()
+	appUser.outputUserValues()
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
